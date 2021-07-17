fe7f5db42a023400cc3ff81163ab60f1f3ae29d2
Storm Collins <collins.storm@gmail.com>
2021-07-17 13:27:17 +0200

    Added unit tests for Curve class

--- src/Curves/Curve.py
+++ src/Curves/Curve.py
@@ -5,21 +5,24 @@ class Curve:
     """
     Base class from which other curves inherit.
     """
-    def __init__(self, **kwargs: list(float)):
-        self.tenors: list(float) = kwargs.pop('tenors', None)
-        self.discount_factors: list(float) = kwargs.pop('discount_factors', None)
+    def __init__(self, **kwargs):
+        self.tenors: np.ndarray = kwargs.pop('tenors', None)
+        self.discount_factors: np.ndarray = kwargs.pop('discount_factors', None)
 
         if not self.tenors.__contains__(0):
             self.tenors = np.append(0, self.tenors)
             self.discount_factors = np.append(1, self.discount_factors)
 
-    def get_discount_factors(self, tenors: float | list(float)) -> float | list(float):
+    def get_discount_factors(self, tenors: np.ndarray) -> np.ndarray:
         """
         Returns discount factors for a list of tenor(s).
+
+        Perform linear interpolation and flat extrapolation.
         :param tenors: Tenor(s) for which to interpolate.
-        :type tenors: float | list(float)
-        :return:
-        :rtype: float | list(float)
+        :type tenors: np.ndarray
+        :return: Array of discount factors.
+        :rtype: np.ndarray
         """
         # TODO: Make interpolation configurable.
+        # TODO: Make extrapolation configurable.
         return np.interp(tenors, self.tenors, self.discount_factors)
--- test/TestsCurves/TestCurve.py
+++ test/TestsCurves/TestCurve.py
@@ -1,4 +1,24 @@
+import numpy as np
 from src.Curves.Curve import Curve
 
 
-class
+def test_discount_factor_at_time_zero():
+    curve = Curve(tenors=[0.25, 0.50, 0.75, 1.00], discount_factors=[0.95, 0.90, 0.85, 0.80])
+    assert curve.get_discount_factors(0) == 1
+
+
+def test_multi_point_discount_factors():
+    curve = Curve(tenors=[0.25, 0.50, 0.75, 1.00], discount_factors=[0.95, 0.90, 0.85, 0.80])
+    assert np.array_equiv(
+        curve.get_discount_factors(np.array([0.25, 0.375, 0.5, 0.625])),
+        np.array([0.95, 0.925, 0.90, 0.875]))
+
+
+def test_discount_factor_right_extrapolation():
+    curve = Curve(tenors=[0.25, 0.50, 0.75, 1.00], discount_factors=[0.95, 0.90, 0.85, 0.80])
+    assert curve.get_discount_factors(1.25) == 0.80
+
+
+def test_discount_factor_left_extrapolation():
+    curve = Curve(tenors=[0.25, 0.50, 0.75, 1.00], discount_factors=[0.95, 0.90, 0.85, 0.80])
+    assert curve.get_discount_factors(0.125) == 0.975
